443b66551229ee475393504f8bd2ace0a1c2afa2
hablijack <christoph.habel@posteo.de>
2022-09-30 07:51:21 +0200

    1.0.28: Frontend for satelites

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,58 +1,104 @@
 <template>
   <v-container fluid>
     <v-row dense>
-      <v-col v-for="sensor in sensors.slice(0,4)" v-bind:key="sensor.name" cols="12" sm="12" md="6" lg="3">
-        <MeasureCard 
+      <v-col
+        v-for="sensor in sensors.slice(0, 4)"
+        v-bind:key="sensor.name"
+        cols="12"
+        sm="12"
+        md="6"
+        lg="3"
+      >
+        <MeasureCard
           :headline="sensor.name"
           :measurement="measurements[sensor.identifier]"
           :unit="sensor.unit"
-          :description="sensor.description" 
+          :description="sensor.description"
           :icon="sensor.icon"
-          color="#5cad8a" />
+          color="#5cad8a"
+        />
       </v-col>
     </v-row>
     <v-row dense>
-      <v-col cols="12" sm="12" md="12" lg="9">
+      <v-col cols="12" sm="12" md="12" lg="9" style="position: relative">
+        <v-row
+          dense
+          justify="end"
+          style="
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            padding: 10px;
+            z-index: 20;
+            width: 100%;
+          "
+        >
+          <v-col v-for="relay in relays" v-bind:key="relay.name">
+            <MiniRelay
+              :color="relay.value ? relay.color : '#343a40'"
+              :name="relay.identifier"
+              :icon="relay.icon"
+            />
+          </v-col>
+        </v-row>
         <v-card img="/img/webcam_current.jpg" min-height="632" />
       </v-col>
       <v-col cols="12" sm="12" md="12" lg="3">
         <v-row dense>
-          <v-col v-for="sensor in sensors.slice(4,8)" v-bind:key="sensor.name" cols="12" sm="12" md="6" lg="12">
-            <MeasureCard 
+          <v-col
+            v-for="sensor in sensors.slice(4, 8)"
+            v-bind:key="sensor.name"
+            cols="12"
+            sm="12"
+            md="6"
+            lg="12"
+          >
+            <MeasureCard
               :headline="sensor.name"
               :measurement="measurements[sensor.identifier]"
               :unit="sensor.unit"
-              :description="sensor.description" 
+              :description="sensor.description"
               :icon="sensor.icon"
-              color="#5cad8a" />
+              color="#5cad8a"
+            />
           </v-col>
         </v-row>
       </v-col>
     </v-row>
     <v-row dense>
-      <v-col v-for="sensor in sensors.slice(8,12)" v-bind:key="sensor.name" cols="12" sm="12" md="6" lg="3">
-        <MeasureCard 
+      <v-col
+        v-for="sensor in sensors.slice(8, 12)"
+        v-bind:key="sensor.name"
+        cols="12"
+        sm="12"
+        md="6"
+        lg="3"
+      >
+        <MeasureCard
           :headline="sensor.name"
           :measurement="measurements[sensor.identifier]"
           :unit="sensor.unit"
-          :description="sensor.description" 
+          :description="sensor.description"
           :icon="sensor.icon"
-          color="#5cad8a" />
+          color="#5cad8a"
+        />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import MiniRelay from "~/components/MiniRelay.vue";
 export default {
-  name: 'Dashboard',
-  middleware: ['auth'],
+  name: "Dashboard",
+  middleware: ["auth"],
   data: () => ({
     measurements: {},
   }),
   async asyncData({ $axios, $config }) {
     const sensors = await $axios.$get(`https://${$config.domain}/api/sensors`);
-    return { sensors };
+    const relays = await $axios.$get(`https://${$config.domain}/api/relays`);
+    return { sensors, relays };
   },
   methods: {
     updateMeasurements(measurements) {
@@ -60,11 +106,14 @@ export default {
     },
   },
   mounted() {
-    const socket = new WebSocket(`wss://${this.$config.domain}/api/sensors/measurements/socket`);
+    const socket = new WebSocket(
+      `wss://${this.$config.domain}/api/sensors/measurements/socket`
+    );
     let nuxtPage = this;
     socket.onmessage = function (message) {
       nuxtPage.updateMeasurements(JSON.parse(message.data));
     };
   },
+  components: { MiniRelay },
 };
 </script>
--- pages/satelites.vue
+++ pages/satelites.vue
@@ -1,12 +1,41 @@
 <template>
   <v-container fluid>
-    Gewächshaus-Sateliten
+    <v-row dense>
+      <v-col cols="12" sm="12" md="12" lg="4">
+        <v-card>
+          <v-card-title>Sateliten:</v-card-title>
+          <v-card-text>
+            <client-only>
+              <TreeChart v-bind="architecture" :json="architecture" />
+            </client-only>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: 'Sateliten',
-  middleware: ['auth'],
+  name: "Sateliten",
+  data() {
+    return {
+      architecture: {
+        name: "Server",
+        image_url: "server.png",
+        class: ["rootNode"],
+        ip: "192.168.178.29:5550",
+        online: true,
+        children: [],
+      },
+    };
+  },
+  async fetch() {
+    const satelites = await this.$axios.$get(
+      `https://${this.$config.domain}/api/satelites`
+    );
+    this.architecture.children = satelites;
+  },
+  middleware: ["auth"],
 };
 </script>
